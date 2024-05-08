029c07225d55593ee38c1bc3fcce2a8a1dd6cdc6
Jens Axel Søgaard <jensaxel@soegaard.net>
2024-05-08 17:09:19 +0200

    Added binaries for arm macs (poppler-aarch64-macosx). Thanks to mflatt.

---
+++ poppler-aarch64-macosx/racket-poppler/info.rkt
@@ -0,0 +1,7 @@
+#lang setup/infotab
+;; SPDX-License-Identifier: (Apache-2.0 OR MIT)
+;; THIS FILE IS AUTO-GENERATED FROM racket/src/native-libs/install.rkt
+
+(define install-platform "aarch64-macosx")
+
+(define copy-foreign-libs '("libpoppler-glib.8.dylib" "libpoppler.44.dylib"))
